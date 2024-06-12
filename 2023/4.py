91c0705ebb4de33ab6b97aed81e9853bbb2c5957
shekhuverma <shekharvrm47@gmail.com>
2024-06-12 21:35:39 +0530

    Added day 4 solution

--- 2023/4.py
+++ 2023/4.py
@@ -5,37 +5,39 @@ with open(FILE_NAME) as f:
 
 result = 0
 
+
 for n, card in enumerate(data, start=1):
     temp = 0
     temp1 = 0
     card = card.split(":")[1].split("|")
     winning_numbers = card[0].split()
     card = card[-1].split()
-    # print("___________")
     for a in winning_numbers:
         if a in card:
             temp += 1
-            # print(f"temp = {temp}")
-    # print(f"Result = {result}")
     if temp != 0:
         result += 2 ** (temp - 1)
 
 print(result)
 
+
+def number_of_winning_card(winning_numbers: list, card: list) -> int:
+    card_copies = 0
+    for a in winning_numbers:
+        if a in card:
+            card_copies += 1
+    return card_copies
+
+
 # part 2
-card_copies = {1: 1}
-for n, card in enumerate(data, start=1):
-    # card_copies[n + 1] = 0
+result = 0
+card_copies = [1] * len(data)
+for n, card in enumerate(data):
     card = card.split(":")[1].split("|")
     winning_numbers = card[0].split()
     card = card[-1].split()
-    j = 1
-    for a in winning_numbers:
-        if a in card:
-            try:
-                card_copies[n + j] += 1
-            except KeyError:
-                card_copies[n + j] = 1
-            j += 1
+    for j in range(number_of_winning_card(winning_numbers, card)):
+        card_copies[n + 1 + j] += card_copies[n]
 
-    print(card_copies)
+result = sum(card_copies)
+print(result)
